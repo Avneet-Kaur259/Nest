93bce454f491ff57235355aa2ea52d401e28acd8
Avneet Kaur <avneet2528@gmail.com>
2025-11-02 23:26:38 +0530

    mongodb connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -23,4 +24,8 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server running on port: " + PORT);
+    });
+});
