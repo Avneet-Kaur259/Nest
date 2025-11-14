de5c4bd4127ed5e48d9e198de39b104d1a7642f4
Avneet Kaur <avneet2528@gmail.com>
2025-11-14 13:30:53 +0530

    code refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-import "dotenv/config";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -48,7 +48,7 @@ export const signup = async (req, res) => {
             });
 
             try {
-                await sendWelcomeEmail(savedUser.email, savedUser.fullName, process.env.CLIENT_URL);
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
             } catch (error) {
                 console.error("Failed to send welcome email:", error);
             }
