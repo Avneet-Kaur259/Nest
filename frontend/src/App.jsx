d3732219144441a75ada63c3154da0c5dc3fcd9b
Avneet Kaur <avneet2528@gmail.com>
2025-12-02 14:49:07 +0530

    auth feature added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import ChatPage from "./pages/ChatPage"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
+import { useAuthStore } from "./store/useAuthStore"
+import { useEffect } from "react"
+import PageLoader from "./components/pageLoader"
 
 const App = () => {
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log({ authUser });
+
+  if (isCheckingAuth) return <PageLoader />
+
   return (
     <div className="min-h-screen relative flex items-center justify-center p-4 overflow-hidden">
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to={"/"} />} />
       </Routes>
     </div>
   )
