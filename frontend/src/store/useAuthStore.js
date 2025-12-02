198280427e8371e67ccea505dfc12306b4833e15
Avneet Kaur <avneet2528@gmail.com>
2025-12-02 14:46:46 +0530

    store created for auth and signup feature added

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { api } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
+
+    checkAuth: async () => {
+        try {
+            const res = await api.get("/auth/me");
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await api.post("/auth/signup", data);
+            set({ authUser: res.data });
+
+            toast.success("Account created successfully!");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+}))
